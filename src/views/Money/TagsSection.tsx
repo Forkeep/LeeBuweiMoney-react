9e2278d0d6824318510bb6a5d94a5dfa455994bc
Forkeep <13126593533@163.com>
2020-05-21 08:23:15 +0800

    标签选择+新增功能

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, {useState} from 'react';
 
 const Wrapper = styled.section`
   background-color: white;
@@ -10,6 +10,7 @@ const Wrapper = styled.section`
   align-items: flex-start;
   >ol{
   display: flex;
+  flex-wrap: wrap;
     >li{
       background-color: #ddd;
       border-radius: 16px;
@@ -17,6 +18,9 @@ const Wrapper = styled.section`
       padding: 2px 1em;
       text-align: center;
       font-size: 14px;
+      &.selected{
+        background-color: red;
+      }
     }
   }
   >button{
@@ -30,17 +34,33 @@ const Wrapper = styled.section`
 `;
 
 const TagsSection: React.FunctionComponent = () => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const addTag = () => {
+    const newTag = window.prompt('请输入新增的标签:');
+    if (newTag !== null) {setTags([...tags, newTag]);}
+  };
+  const toggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
   return (
     <Wrapper>
       <ol>
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
+        {tags.map(tag =>
+          <li key={tag}
+              onClick={() => {toggleTag(tag);}}
+              className={selectedTags.indexOf(tag) >= 0 ? 'selected' : ''}
+          >{tag}</li>
+        )}
       </ol>
-      <button>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </Wrapper>
-  )
+  );
 };
 
 
